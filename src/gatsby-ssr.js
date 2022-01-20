889c5eabdd9f3486e9f246ef20fac27fade8d9ec
abdelrahman.osama <abdelrahman.osama@bitspire.ca>
2022-01-20 15:22:24 +0000

    modify gatsby-ssr

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,20 +1,18 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 
-const JSTrackerFile = ({ hostname, version }) => (
+const JSTrackerFile = () => (
   <script
     type="text/javascript"
     dangerouslySetInnerHTML={{
       __html: `;(function(p,l,o,w,i,n,g){if(!p[i]){p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[];
 p.GlobalSnowplowNamespace.push(i);p[i]=function(){(p[i].q=p[i].q||[]).push(arguments)
 };p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[0];n.async=1;n.crossorigin="anonymous";
-n.src=w;g.parentNode.insertBefore(n,g)}}(window,document,"script","${hostname}/${version}/sp.js","snowplow"));
+n.src=w;g.parentNode.insertBefore(n,g)}}(window,document,"script","https://gitlab.beyond.cc/assets/snowplow/sp-bc5b4b4067898d2d20c35fec045d91d032cb739c3deab5f42607edbeca08323a.js","snowplow"));
       `,
     }}
   />
 );
-JSTrackerFile.propTypes = { hostname: PropTypes.string, version: PropTypes.string };
-JSTrackerFile.defaultProps = { hostname: '//d1fc8wv8zag5ca.cloudfront.net', version: '2.9.0' };
 
 const SnowplowInstall = ({ namespace, collector, config }) => (
   <script
@@ -27,9 +25,9 @@ const SnowplowInstall = ({ namespace, collector, config }) => (
 SnowplowInstall.propTypes = { namespace: PropTypes.string, collector: PropTypes.string, config: PropTypes.object };
 SnowplowInstall.defaultProps = { namespace: 'cf', collector: '', config: {} };
 
-exports.onRenderBody = ({ setHeadComponents }, { hostname, version, namespace, collector, config }) => {
+exports.onRenderBody = ({ setHeadComponents }, { namespace, collector, config }) => {
   return setHeadComponents([
-    <JSTrackerFile hostname={hostname} version={version} key="gatsby-plugin-snowplow-jsfile" />,
+    <JSTrackerFile key="gatsby-plugin-snowplow-jsfile" />,
     <SnowplowInstall
       namespace={namespace}
       collector={collector}
